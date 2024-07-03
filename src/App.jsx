fb985ea25667688e0ea6dca0896c9a818dadb1e4
aathish007 <mailforaathish@gmail.com>
2024-07-03 14:49:42 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDoComponent from './Components/ToDoComponent/ToDoComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <ToDoComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
